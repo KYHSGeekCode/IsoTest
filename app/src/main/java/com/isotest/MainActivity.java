f285ffd4d5490b37d595425119781a35835bd1ef
Hyeonseo Yang <jourhyang123@gmail.com>
2020-12-20 20:16:23 +0900

    :tada: Startt project

--- app/src/main/java/com/isotest/MainActivity.java
+++ app/src/main/java/com/isotest/MainActivity.java
@@ -2,6 +2,7 @@ package com.isotest;
 
 import android.app.*;
 import android.os.*;
+import io.fabianterhorst.isometric.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +11,10 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		iview=(IsometricView) findViewById(R.id.isometricView);
+		Path path=new Path();
+		iview.add(path,new Color(1,2,3));
+		
     }
+	IsometricView iview;
 }
